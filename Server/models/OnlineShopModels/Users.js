2b1425f45950be36738aa88c1069f58911c82d61
Dilshan Pathum <pathumpanagoda@gmail.com>
2025-09-24 17:03:16 +0530

    Injection - FIXED

--- Server/models/OnlineShopModels/Users.js
+++ Server/models/OnlineShopModels/Users.js
@@ -20,36 +20,6 @@ const UsersSchema = mongoose.Schema({
     cartData:{
         type:Object,   
     },
-    // Account security fields
-    loginAttempts: {
-        type: Number,
-        default: 0
-    },
-    lockUntil: {
-        type: Date
-    },
-    lastLogin: {
-        type: Date
-    },
-    isLocked: {
-        type: Boolean,
-        default: false
-    },
-    // MFA fields
-    mfaSecret: {
-        type: String
-    },
-    mfaEnabled: {
-        type: Boolean,
-        default: false
-    },
-    // Password reset
-    resetPasswordToken: {
-        type: String
-    },
-    resetPasswordExpires: {
-        type: Date
-    },
     date:{
         type:Date,
         default:Date.now,
@@ -74,64 +44,6 @@ UsersSchema.pre('save', async function(next) {
 // Method to compare password
 UsersSchema.methods.comparePassword = async function(candidatePassword) {
     return bcrypt.compare(candidatePassword, this.password);
-};
-
-// Account lockout methods
-UsersSchema.virtual('isLocked').get(function() {
-    return !!(this.lockUntil && this.lockUntil > Date.now());
-});
-
-UsersSchema.methods.incLoginAttempts = function() {
-    // If we have a previous lock that has expired, restart at 1
-    if (this.lockUntil && this.lockUntil < Date.now()) {
-        return this.updateOne({
-            $unset: { lockUntil: 1 },
-            $set: { loginAttempts: 1 }
-        });
-    }
-    
-    const updates = { $inc: { loginAttempts: 1 } };
-    
-    // Lock account after 5 failed attempts for 2 hours
-    if (this.loginAttempts + 1 >= 5 && !this.isLocked) {
-        updates.$set = { lockUntil: Date.now() + 2 * 60 * 60 * 1000 }; // 2 hours
-    }
-    
-    return this.updateOne(updates);
-};
-
-UsersSchema.methods.resetLoginAttempts = function() {
-    return this.updateOne({
-        $unset: { loginAttempts: 1, lockUntil: 1 },
-        $set: { lastLogin: new Date() }
-    });
-};
-
-// MFA methods
-UsersSchema.methods.generateMfaSecret = function() {
-    const secret = speakeasy.generateSecret({
-        name: `Vehicle Services (${this.email})`,
-        length: 32
-    });
-    this.mfaSecret = secret.base32;
-    return secret;
-};
-
-UsersSchema.methods.verifyMfaToken = function(token) {
-    return speakeasy.totp.verify({
-        secret: this.mfaSecret,
-        encoding: 'base32',
-        token: token,
-        window: 2
-    });
-};
-
-// Password reset methods
-UsersSchema.methods.generatePasswordResetToken = function() {
-    const resetToken = uuidv4();
-    this.resetPasswordToken = resetToken;
-    this.resetPasswordExpires = Date.now() + 10 * 60 * 1000; // 10 minutes
-    return resetToken;
 };
 
 module.exports = mongoose.model("Users", UsersSchema);
